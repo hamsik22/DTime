a6af6b073e19fc0d88dd859a2d3d2e35f39fcad6
HwangSukHyeon <payback9488@gmail.com>
2024-06-18 00:37:30 +0900

    [File&View]: TimeListCell, DummyDataModel
    - List 형태의 뷰를 만들기 위해 TimeListCell을 생성
    - title, description, date 등 변수를 가진 구조체를 형성(DataModel)
        + 초기 작업이라 구조체 이름을 대충 지었음
    - dummyDataArray를 통해 List View를 프리뷰로 확인
    ---------------------- TODO ------------------------
    - 컨텐츠 내용에 따른 뷰 레이아웃 최적화
    - D-day 계산 로직 구현 및 적용

---
+++ DTime/TimeListCell.swift
@@ -0,0 +1,18 @@
+//
+//  TimeListCell.swift
+//  DTime
+//
+//  Created by 황석현 on 6/17/24.
+//
+
+import SwiftUI
+
+struct TimeListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeListCell()
+}

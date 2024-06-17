abdc3162ddef81c273017ea988b8a621936622e6
HwangSukHyeon <payback9488@gmail.com>
2024-06-17 21:14:54 +0900

    First Commit

---
+++ DTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DTime
+//
+//  Created by 황석현 on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DTime/DTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTimeApp.swift
+//  DTime
+//
+//  Created by 황석현 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
